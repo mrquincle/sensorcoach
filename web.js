7f991555238c8edabd2fad59650f6b1ef3764943
Anne van Rossum <anne@almende.org>
2013-07-05 14:31:39 +0200

    Get rid of bird and use oauth directly

--- web.js
+++ web.js
@@ -1,15 +1,12 @@
 var express = require('express'),
-    qs = require('querystring'),
+    sys = require('util'),
+    oauth = require('oauth'),
     SendGrid = require('sendgrid').SendGrid,
     port = process.env.PORT || 5000;
 
-var Bird = require('bird');
-var oauth = {
-  oauth_token : process.env.TWITTER_CONSUMER_KEY,
-  oauth_token_secret : process.env.TWITTER_CONSUMER_SECRET,
-  callback: 'http://sensorcoach.heroku.com/callback'
-};
-
+var _twitterConsumerKey = process.env['TWITTER_CONSUMER_KEY'];
+var _twitterConsumerSecret = process.env['TWITTER_CONSUMER_SECRET'];
+ 
 var app = module.exports = express();
 app.use(express.logger());
 
@@ -22,66 +19,57 @@ app.use(express.session({secret: "aha-erlebniss"}));
 app.use(express.methodOverride());
 app.use(app.router);
 
+app.use(function(req, res){
+    res.locals = req.session;
+});
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-app.get('/', function(req, res){
-  res.send('<a href="/login">Login</a><br><a href="/home_timeline.json">Home Timeline</a>');
-});
+function consumer() {
+  return new oauth.OAuth(
+    "https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token", 
+    _twitterConsumerKey, _twitterConsumerSecret, "1.0A", "http://sensorcoach.heroku.com/sessions/callback", "HMAC-SHA1");   
+}
 
 
-app.get('/login', function(req, res) {
-  req.session.oauth = oauth;
-  Bird.login(req.session.oauth, function(err,r,body){
-    if (err) {
-      //add error handling here for when twitter returns an error
-      res.send("Error getting OAuth request token");
-    } else {
-      //parse tokens received from twitter
-      var tokens = qs.parse(body);
-      //set temporary oauth tokens for the users session
-      req.session.oauth.token = tokens.oauth_token;
-      req.session.oauth.token_secret = tokens.oauth_token_secret;
-
-      //redirect user to authorize with temporary token
-      res.redirect("https://twitter.com/oauth/authorize?oauth_token=" + tokens.oauth_token);
+app.get('/sessions/connect', function(req, res){
+  consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
+    if (error) {
+      res.send("Error getting OAuth request token : " + sys.inspect(error), 500);
+    } else {  
+      req.session.oauthRequestToken = oauthToken;
+      req.session.oauthRequestTokenSecret = oauthTokenSecret;
+      res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
     }
   });
 });
 
-app.get('/logout', function(req, res){
-  req.session.destroy();
-  res.redirect('/');
-});
 
-app.get('/callback', function(req, res){
-  //set oauth verifier for the users session
-  req.session.oauth.verifier = req.query.oauth_verifier;
-  Bird.auth_callback(req.session.oauth, function(err, r, body){
-    if (err) {
-      //add error handling here for when twitter returns an error
-      res.send(err);
+app.get('/sessions/callback', function(req, res){
+  sys.puts(">>"+req.session.oauthRequestToken);
+  sys.puts(">>"+req.session.oauthRequestTokenSecret);
+  sys.puts(">>"+req.query.oauth_verifier);
+  consumer().getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
+    if (error) {
+      res.send("Error getting OAuth access token : " + sys.inspect(error) + "["+oauthAccessToken+"]"+ "["+oauthAccessTokenSecret+"]"+ "["+sys.inspect(results)+"]", 500);
     } else {
-      //parse tokens received from twitter
-      var tokens = qs.parse(body);
-      //set permanent access tokens for the users session
-      req.session.oauth.token = tokens.oauth_token;
-      req.session.oauth.token_secret = tokens.oauth_token_secret;
-
-      //redirect the user
-      res.redirect('/');
+      req.session.oauthAccessToken = oauthAccessToken;
+      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
+      // Right here is where we would write out some nice user stuff
+      consumer().get("http://twitter.com/account/verify_credentials.json", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
+        if (error) {
+          res.send("Error getting twitter screen name : " + sys.inspect(error), 500);
+        } else {
+          console.log("data is %j", data);
+          data = JSON.parse(data);
+          req.session.twitterScreenName = data["screen_name"];    
+          res.send('You are signed in: ' + req.session.twitterScreenName)
+        }  
+      });  
     }
   });
-});
-
-//route using a stream
-app.get('/home_timeline.json', function(req, res){
-  var options = req.query;
-  options.oauth = req.session.oauth;
-
-  //Bird.home_timeline will return a stream that can be piped to a response
-  Bird.home_timeline(options).pipe(res);
 });
 
 app.post('/contact', function(req, res) {
